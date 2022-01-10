8e0275c5d8aceb9ff164ec470d52f4b8110698f1
jccruzgit <jcrivcruz@gmail.com>
2022-01-09 22:29:56 -0600

    first commit

---
+++ SpringBootApp/src/test/java/sv/com/sertracen/model/SpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package sv.com.sertracen.model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
